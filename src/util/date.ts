6fcd3ee355ea525a37f370c7c09860992bce93f5
Mateo <cuervo.taylor@gmail.com>
2019-05-18 14:14:45 +0200

    npm audit fix

--- src/util/date.ts
+++ src/util/date.ts
@@ -1,3 +1,6 @@
+export const dateParse = (s: string): Date => {
+    return new Date(Date.parse(s));
+};
 // Returns dd.mm.yyyy
 export const dateFormat = (d: Date): string => {
     // ISO without timestamp is YYYY-MM-DD
