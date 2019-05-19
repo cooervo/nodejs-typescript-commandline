8c88cd28effe61cd80b872b6d2426366b6c3df45
Mateo <cuervo.taylor@gmail.com>
2019-05-19 11:59:55 +0200

    minor refactor

--- src/app.ts
+++ src/app.ts
@@ -7,36 +7,49 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+const askYear = () => {
+    rl.question("Write year to report (format as YYYY): ", (input: string) => {
+        if (!isValid(input)) {
+            askYear();
+            return;
+        }
+
+        const yearInput: Date = new Date(`${Number(input)} 12 31`);
+        writeReport(yearInput, User.getUsers(yearInput));
+        rl.close();
+    });
+};
+
+// Cleans report.txt file and writes employees names and vacation days
 const writeReport = (yearInput: Date, users: User.User[]) => {
     const filePath = "./report.txt";
     File.cleanFile(filePath);
+
+    // Write title
     let appendTxt = `Vacation days for year: ${yearInput.getFullYear()}\n\n`;
     File.appendText(filePath, appendTxt);
+
+    // Write content
     users.forEach(u => {
         appendTxt = `Employee: ${u.name}, vacation days: ${u.getVacationsDaysWithPolicy(yearInput)}.\n`;
         File.appendText(filePath, appendTxt);
     });
 };
 
-const askYear = () => {
-    rl.question("Write year to report (format as YYYY): ", (input: string) => {
-        if (isNaN(Number(input))) {
-            console.log("Error: Please try again, with a valid number.\n");
-            askYear();
-            return;
-        }
-
-        if (input.length !== 4) {
-            console.log("Error: Year should be 4 digits. Example: 1950, 2019.\n");
-            askYear();
-            return;
-        }
-
-        const yearInput: Date = new Date(`${Number(input)} 12 31`);
-        const users = User.getUsers(yearInput);
-        writeReport(yearInput, users);
-        rl.close();
-    });
-};
+/**
+ * Validate input lenght and if it is a number
+ * @param input
+ */
+function isValid(input: string) {
+    if (isNaN(Number(input))) {
+        console.log("Error: Please try again, with a valid number.\n");
+        return false;
+    }
+    if (input.length !== 4) {
+        console.log("Error: Year should be 4 digits. Example: 1950, 2019.\n");
+        return false;
+    }
+    return true;
+}
 
 askYear();
